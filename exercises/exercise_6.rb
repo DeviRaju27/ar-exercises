3fa08f5dcc154d31c8b7b015b6999b234d4724e0
Devi Raju <deviraju999@gmail.com>
2022-11-14 23:53:52 -0800

    add validate

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,6 +9,6 @@ puts "Exercise 6"
 puts "----------"
 
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store2.employees.create(first_name: "Daniel", last_name: "Lewett", hourly_rate: 20)
+@store2.employees.create(first_name: "Daniel", last_name: "Lewett", hourly_rate: 70)
 @store1.employees.create(first_name: "Sherly", last_name: "Monk", hourly_rate: 40)
 @store2.employees.create(first_name: "Malaka", last_name: "Disher", hourly_rate: 50)
